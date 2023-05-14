6100ed6e95ed53152ec19f1573cf8327afa32698
دابس بالعربي <arabicdapps@gmail.com>
2023-05-14 11:17:26 +0300

    add logo

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,12 +1,13 @@
+const Token = artifacts.require("Token");
 
-   
 const NFTMarketPlace = artifacts.require("NFTMarketPlace");
 const NFT = artifacts.require("NFT");
 
+module.exports = async function (deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
 
-module.exports =  async function (deployer) {
-  
   await deployer.deploy(NFTMarketPlace);
-  const market = await NFTMarketPlace.deployed()
-  await deployer.deploy(NFT,market.address);
-}
+  const market = await NFTMarketPlace.deployed();
+  await deployer.deploy(NFT, market.address);
+};
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
 import { useState } from "react";
-import Header from './../components/header'
-import Footer from './../components/footer'
-import Paragraph1 from './../components/index/paragraph1'
-import Paragraph2 from './../components/index/paragraph2'
-import Paragraph3 from './../components/index/paragraph3'
-import Paragraph4 from './../components/index/paragraph4'
+import Header from "./../components/header";
+import Footer from "./../components/footer";
+import Paragraph1 from "./../components/index/paragraph1";
+import Paragraph2 from "./../components/index/paragraph2";
+import Paragraph3 from "./../components/index/paragraph3";
+import Paragraph4 from "./../components/index/paragraph4";
 
 export default function ExplorePage() {
-  const [themeMode, setThemeMode] = useState(true)
+  const [themeMode, setThemeMode] = useState(true);
 
   return (
     <>
       <Head>
-        <title>OpenSky</title>
+        <title>CHINA LADY</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <Header current={0}></Header>
-      <div className='bg-[#0D0F23] dark:bg-white'>
-        <div className='w-full 2xl:max-w-screen-2xl h-auto pt-[104px] flex flex-col m-auto'>
+      <div className="bg-[#0D0F23] dark:bg-white">
+        <div className="w-full 2xl:max-w-screen-2xl h-auto pt-[104px] flex flex-col m-auto">
           <Paragraph1></Paragraph1>
           <Paragraph2></Paragraph2>
           <Paragraph3></Paragraph3>
@@ -28,5 +28,5 @@ export default function ExplorePage() {
       </div>
       <Footer></Footer>
     </>
-  )
+  );
 }
